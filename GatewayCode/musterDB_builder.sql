35da6e20de00bc721ae555f79ade2a005b305d43
mpakin1 <mpakin1@nps.edu>
2016-08-25 10:24:11 -0700

    Upload changes to SQL loader

--- GatewayCode/musterDB_builder.sql
+++ GatewayCode/musterDB_builder.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 5.7.13, for Linux (x86_64)
 --
--- Host: localhost    Database: musterWebpage
+-- Host: localhost    Database: muster
 -- ------------------------------------------------------
 -- Server version	5.7.13-0ubuntu0.16.04.2
 
@@ -31,7 +31,7 @@ CREATE TABLE `devices` (
   UNIQUE KEY `studentDevice_MAC_uindex` (`MAC`),
   KEY `studentDevice_students_ID_fk` (`studentID`),
   CONSTRAINT `studentDevice_students_ID_fk` FOREIGN KEY (`studentID`) REFERENCES `students` (`ID`) ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -40,7 +40,7 @@ CREATE TABLE `devices` (
 
 LOCK TABLES `devices` WRITE;
 /*!40000 ALTER TABLE `devices` DISABLE KEYS */;
-INSERT INTO `devices` VALUES (1,'1:2:3:4',1,NULL);
+INSERT INTO `devices` VALUES (1,'1:2:3:4',1,'Test'),(2,'70:3E:AC:D8:7F:26',2,'Phone'),(3,'DC:37:14:34:C8:F9',3,'Phone'),(4,'D0:59:E4:95:87:64',4,'Phone'),(5,'8C:29:37:1C:FC:63',1,'Phone'),(6,'9C:FC:01:BF:54:68',5,'Phone'),(7,'F4:42:8F:62:C5:32',6,'Phone'),(8,'B8:5A:73:14:40:EC',7,'Phone'),(9,'10:41:7F:1E:CB:BA',8,'Phone');
 /*!40000 ALTER TABLE `devices` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -60,7 +60,7 @@ CREATE TABLE `musters` (
   UNIQUE KEY `musters_ID_uindex` (`ID`),
   KEY `musters_studentDevice_ID_fk` (`deviceID`),
   CONSTRAINT `musters_studentDevice_ID_fk` FOREIGN KEY (`deviceID`) REFERENCES `devices` (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -69,7 +69,7 @@ CREATE TABLE `musters` (
 
 LOCK TABLES `musters` WRITE;
 /*!40000 ALTER TABLE `musters` DISABLE KEYS */;
-INSERT INTO `musters` VALUES (1,'2016-08-19 01:48:05',1,'test1');
+INSERT INTO `musters` VALUES (24,'2016-08-23 17:49:08',1,'Node1');
 /*!40000 ALTER TABLE `musters` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -88,7 +88,7 @@ CREATE TABLE `students` (
   PRIMARY KEY (`ID`),
   UNIQUE KEY `students_ID_uindex` (`ID`),
   UNIQUE KEY `students_email_uindex` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -97,7 +97,7 @@ CREATE TABLE `students` (
 
 LOCK TABLES `students` WRITE;
 /*!40000 ALTER TABLE `students` DISABLE KEYS */;
-INSERT INTO `students` VALUES ('Micah','Akin','mpakin1@nps.edu',1);
+INSERT INTO `students` VALUES ('Micah','Akin','mpakin1@nps.edu',1),('Mike','Harris','dmharris@nps.edu',2),('Andrew','Delding','arbelding@nps.edu',3),('Paul','Haagenson','pdhaagen@nps.edu',4),('Victor','Castro','vgcastro@nps.edu',5),('Raven','Holm','rrholm@nps.edu',6),('Steven','Thompson','skthomps@nps.edu',7),('Devin','Smiley','dsmiley@nps.edu',8);
 /*!40000 ALTER TABLE `students` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -110,4 +110,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-08-19  0:50:56
+-- Dump completed on 2016-08-23 13:12:29
